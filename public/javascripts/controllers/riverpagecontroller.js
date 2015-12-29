8b1eed73b5da5408ac91024f2c9dd0e9f8fa2ed0
paulsiskind <paulsiskind@gmail.com>
2015-12-28 21:17:31 -0500

    Added to json

--- public/javascripts/controllers/riverpagecontroller.js
+++ public/javascripts/controllers/riverpagecontroller.js
@@ -14,9 +14,10 @@ app.controller("RiverPageController", function($scope, $http, $routeParams){
       console.log(response.data.value.timeSeries[0].values[0].value[0].value)
         $scope.flows = response.data.value
     })
-          $http.get("http://api.wunderground.com/api/2dc07206ff5e682e/geolookup/forecast/q/CO/denver.json").then(function(response){
-      console.log(response.data.forecast, "Look Over Here!")
+          $http.get("http://api.wunderground.com/api/2dc07206ff5e682e/geolookup/forecast/q/"+$scope.riverInfo.geometry.coordinates +".json").then(function(response){
+      console.log($scope.riverInfo.geometry.coordinates, "Look Over Here!")
       $scope.weatherUnder = response.data.forecast.txt_forecast.forecastday
+
       // $http.get("http://api.wunderground.com/api/2dc07206ff5e682e/geolookup/forecast/q/" + $scope.riverInfo.latitude +',' + $scope.riverInfo.longitude + ".json").then(function(response){
       // console.log(response.data.forecast, "Look Over Here!")
       // $scope.weatherUnder = response.data.forecast.txt_forecast.forecastday
@@ -24,15 +25,19 @@ app.controller("RiverPageController", function($scope, $http, $routeParams){
     })
   });
 
-  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Series A', 'Series B'];
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July","August","September","October","November","December"];
+  // $scope.series = ['Series A'];
   $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
+    [65, 59, 80, 81, 56, 55, 40,45,55,65,75,80,99,78]
   ];
   $scope.onClick = function (points, evt) {
     console.log(points, evt);
   };
+      $scope.chart = function(select) {
+                
+       $scope.chartSelect = select;
+       console.log($scope.chartSelect)
+  }
 
 
 
