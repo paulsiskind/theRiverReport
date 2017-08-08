bd15b9ae7a02212beda5f32092f08651edab8b6d
paulsiskind <paulsiskind@gmail.com>
2017-08-08 13:20:00 -0600

    MVP of msg service

--- scheduled-job.js
+++ scheduled-job.js
@@ -23,10 +23,10 @@ var app = {
 	},
 	checkIfComplete: function(){
 		if(app.result == true){
-			app.text()
+			app.twil()
      
 		}else{
-			setTimeout(function(){app.checkIfComplete()}, 1000);		
+			setTimeout(function(){app.checkIfComplete()}, 1200);		
 		}
 	},
 
@@ -62,17 +62,26 @@ var app = {
 		var x = http.request(options,function(res){
 		  res.on('data',function(data){   
 		    app.favorites = JSON.parse(data)
+		    app.addToObj()
 		    app.insertFavorites()
 		  });
 		});
 		x.end();
 	},
 
-	insertFavorites: function(){
-  
+	addToObj: function(){
+
+		for(var i=0;i<app.favorites.length; i++){
+			app.favorites[i]['Current'] = 0;
+			app.favorites[i]['in'] = false;
+		}
   	for(var x=0;x<app.users.length;x++){
   		app.users[x].favorites=[];
-  	}
+  	}		
+	},
+
+	insertFavorites: function(){
+  
 		for ( var i = 0; i < app.users.length; i++ ) {
       for ( var e = 0; e < app.favorites.length; e++ ) {
       	var ab = app.users[i].facebookid.slice(1,10)
@@ -91,6 +100,7 @@ var app = {
 
       		if(urid === rdid){     			
       			app.users[i].favorites[y]['riverInfo'] = (app.riverData[p])
+
       		}
       	}
       }
@@ -121,7 +131,7 @@ var app = {
 	},
 
 	getRiverLevels:function(){
-	  app.msgIdeal['Current']=[];
+	 
 
       // start map
     app.users.map(function(favs){
@@ -136,11 +146,11 @@ var app = {
 				  var tacoTruck = JSON.parse(body)
 				  var riverLevel = tacoTruck.value.timeSeries[0].values[0].value[0].value
 				  var ideal = river.riverlevel
-	
+	        console.log(riverLevel)
 				  if(ideal !== null){
 				  	if(riverLevel >= ideal){
-				  	  app.msgIdeal.push(river)
-				  	  app.msgIdeal['Current'].push(riverLevel)
+				  	  river.Current = riverLevel
+				  	  river.in = true;
 				  	};
 				  };
 				  app.everyThingIsDone()
@@ -149,56 +159,39 @@ var app = {
     })
 	},
 
-	text:function(){
-    
-    for(var x=0;x<app.users.length;x++){
-  		app.users[x].ideal=[];
-  	}
-
-		console.log('Texting Happens Here:')
-
-		for ( var i = 0; i < app.users.length; i++ ) {
-      for ( var e = 0; e < app.msgIdeal.length; e++ ) {
-      	// console.log(i, e)
-      	var ab = app.users[i].facebookid.slice(1,10)
-      	var ba = app.msgIdeal[e].facebook_id.slice(1,10)
-        if(ab === ba ){   	
-          app.users[i].ideal.push(app.favorites[e]);
-        }
-        if(e===app.msgIdeal.length){
-        }
-      }
-    }
-      app.twil()
-	},
-
 	twil:function(){
 		console.log("I'm Last!")
 		var user = app.users
-		var greeting = '\n' + '\n' +'\n' +
+		var num = 1;
+		
 
-		               "Greetings, dear customer. I hope you enjoy the river report"+ '\n' +
-		               "This is a laybor of love for the kayaking community" + '\n' +
-		               "Thank You theRiverReport" + '\n'
     for(var i=0; i<user.length;i++){	
-		var tacos = []
-
-    	for(var j=0; j< user[i].ideal.length;j++){
-          tacos.push(user[i].ideal[j])
+    	var msg = []
+    	var sendMsg = [];
+			var flag = false
+    	for(var j=0; j<user[i].favorites.length; j++){
+    		if(user[i].favorites[j].in === true){
+    			console.log(user[i].firstname, num++, user[i].favorites[j].Current, user[i].favorites[j].riverlevel, user[i].favorites[j].riverInfo.name)
+          msg.push(user[i].favorites[j].riverInfo.name + ' is in! ' 
+                              + 'The Current flow is: '+ user[i].favorites[j].Current + '. Your recommended level is: ' + user[i].favorites[j].riverlevel + '.' + '\n')
+         
+    		};
     	};
-     	console.log('sending msg to:', user[i].userphone, tacos, greeting)
-     	if(tacos.length > 0){
-				client.messages.create({
-				    to:'', //user[i].userphone,
-				    from:"+19707103177",
-				    body: tacos,
-				}, function(error, message) {
-				    if (error) {
-				        console.log(error.message);
-				    }
-				  });
+    	var greeting = '\n' + 'Thank you for using theRiverReport'
+	
+    	console.log('sending msg to:', user[i].userphone, msg.join(' '))
+	     	if(msg.length>0){
+					client.messages.create({
+					    to: user[i].userphone, 
+					    from:"+19707103508",
+					    body: msg.join(' ') + greeting,
+					}, function(error, message) {
+					    if (error) {
+					        console.log(error.message);
+					    }
+					  });
+				}
 			}
-  	};
 	},
 
 
