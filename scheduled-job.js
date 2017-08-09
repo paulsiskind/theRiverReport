6aac265a49c44923d09323eda78ed4855b041830
paulsiskind <paulsiskind@gmail.com>
2017-08-08 18:47:11 -0600

    Up to date

--- scheduled-job.js
+++ scheduled-job.js
@@ -1,6 +1,5 @@
 require('dotenv').load()
 var http = require('http');
-var BigNumber = require('bignumber.js');
 var request = require('request');
 var client = require('twilio')(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
@@ -11,27 +10,22 @@ var app = {
 
 	users:[],
 	favorites:[],
-	done: false,
 	riverData:[],
 	msgIdeal:[],
-	flag:false,
 	counter:0,
-	ghost:[],
   
-  everyThingIsDone: function(){
-		
-		
-	if(app.counter === app.users.length){
-			app.result = true;
+  everyThingIsDone: function(){		
+		if(app.counter === app.users.length){
+				app.result = true;
 		}
-		 return app.result
+		return app.result
 	},
 	checkIfComplete: function(){
 		if(app.result == true){
-			app.text()
+			app.twil()
      
 		}else{
-			setTimeout(function(){app.checkIfComplete()}, 1000);		
+			setTimeout(function(){app.checkIfComplete()}, 1200);		
 		}
 	},
 
@@ -46,70 +40,55 @@ var app = {
         accept: 'application/json'
 	    }
 		};
-
-		console.log("Start");
 		var x = http.request(options,function(res){
-		  console.log("Connected");
-		  res.on('data',function(data){
-		    
+		  res.on('data',function(data){    
 		    app.users = JSON.parse(data)
 		    app.getFavorites()
-		    console.log(app.users)
 		  });
 		});
-
 		x.end();
-
-
 	},
-
 	getFavorites: function(){
-    console.log('Heeeelo')
 		var options = {
     host: 'localhost',
     port: 3000,
     path: '/allUsersFavorites/',
     method: 'GET',
-    // 'req.user.facebookid': users[0].facebookid,
 	    headers: {
 	        accept: 'application/json',       
 		  }
 		};
-
-		console.log("Start");
 		var x = http.request(options,function(res){
-		  console.log("Connected Bitches");
-		  res.on('data',function(data){
-		    
+		  res.on('data',function(data){   
 		    app.favorites = JSON.parse(data)
+		    app.addToObj()
 		    app.insertFavorites()
 		  });
 		});
-
 		x.end();
+	},
 
+	addToObj: function(){
+
+		for(var i=0;i<app.favorites.length; i++){
+			app.favorites[i]['Current'] = 0;
+			app.favorites[i]['in'] = false;
+		}
+  	for(var x=0;x<app.users.length;x++){
+  		app.users[x].favorites=[];
+  	}		
 	},
 
 	insertFavorites: function(){
   
-  	for(var x=0;x<app.users.length;x++){
-  		app.users[x].favorites=[];
-  	}
-
-
-		console.log('tacos Trucks', app.favorites.length)
 		for ( var i = 0; i < app.users.length; i++ ) {
       for ( var e = 0; e < app.favorites.length; e++ ) {
-      	// console.log(i, e)
       	var ab = app.users[i].facebookid.slice(1,10)
       	var ba = app.favorites[e].facebook_id.slice(1,10)
-      	// console.log(parseInt(new BigNumber(parseInt(app.users[i].facebookId)), new BigNumber(parseInt(app.favorites[e].facebook_id))))
-        if(ab === ba ){
-        	
+        if(ab === ba ){ 	
           app.users[i].favorites.push(app.favorites[e]);
         }
       }
-    // console.log(app.users)
     }
     var userFav = []
     for ( var i = 0; i < app.users.length; i++ ) {
@@ -117,11 +96,10 @@ var app = {
       	for(var p =0; p<app.riverData.length;p++){
       		var urid = app.users[i].favorites[y].riverid
       		var rdid = app.riverData[p].id
-      		// console.log(urid, rdid, 'beasters')
-      		if(urid === rdid){
-      			
+
+      		if(urid === rdid){     			
       			app.users[i].favorites[y]['riverInfo'] = (app.riverData[p])
-      			// console.log(app.users[i].favorites[y],'Osso dog')
+
       		}
       	}
       }
@@ -144,22 +122,15 @@ var app = {
 
 		console.log("Start");
 		var x = http.request(options,function(res){
-		  // console.log("Connected");
 		  res.on('data',function(data){
-		    
 		    app.riverData = JSON.parse(data)
-		    // console.log(app.riverData)
 		  });
 		});
-
 		x.end();
-
-
-
 	},
 
 	getRiverLevels:function(){
-	  app.msgIdeal['Current']=[];
+	 
 
       // start map
     app.users.map(function(favs){
@@ -174,69 +145,52 @@ var app = {
 				  var tacoTruck = JSON.parse(body)
 				  var riverLevel = tacoTruck.value.timeSeries[0].values[0].value[0].value
 				  var ideal = river.riverlevel
-				 // console.log(riverLevel, ideal);
-
+	        console.log(riverLevel)
 				  if(ideal !== null){
-				  	 // 	app.msgIdeal.push(favs)
-
 				  	if(riverLevel >= ideal){
-				  	//  console.log(riverLevel)
-				  	  app.msgIdeal.push(river)
-				  	  app.msgIdeal['Current'].push(riverLevel)
+				  	  river.Current = riverLevel
+				  	  river.in = true;
 				  	};
 				  };
 				  app.everyThingIsDone()
 				});
       })
     })
-             // app.text();
-	},
-
-	text:function(){
-    
-    for(var x=0;x<app.users.length;x++){
-  		app.users[x].ideal=[];
-  	}
-
-		console.log('Texting Happens Here:')
-
-		for ( var i = 0; i < app.users.length; i++ ) {
-      for ( var e = 0; e < app.msgIdeal.length; e++ ) {
-      	// console.log(i, e)
-      	var ab = app.users[i].facebookid.slice(1,10)
-      	var ba = app.msgIdeal[e].facebook_id.slice(1,10)
-      	// console.log(parseInt(new BigNumber(parseInt(app.users[i].facebookId)), new BigNumber(parseInt(app.favorites[e].facebook_id))))
-        if(ab === ba ){
-        	
-          app.users[i].ideal.push(app.favorites[e]);
-        }
-        if(e===app.msgIdeal.length){
-        }
-      }
-    // console.log(app.users)
-    }
-        	app.twil()
 	},
 
 	twil:function(){
 		console.log("I'm Last!")
-    for(var i=0; i<app.users.length;i++){
-    	//console.log(app.users[i].ideal)
-    	app.ghost = app.users[i].ideal;
-     // if(app.users.hasOwnProperty('ideal')){
-      	console.log('sending msg to:', app.users[i].userphone)
-				client.messages.create({
-				    to: app.users[i].userphone,
-				    from:"+19707103177",
-				    body: 'Tacos love Gore Canyon',
-				}, function(error, message) {
-				    if (error) {
-				        console.log(error.message);
-				    }
-				  });
-		//	}
-		}
+		var user = app.users
+		var num = 1;
+		
 
+    for(var i=0; i<user.length;i++){	
+    	var msg = []
+    	var sendMsg = [];
+			var flag = false
+    	for(var j=0; j<user[i].favorites.length; j++){
+    		if(user[i].favorites[j].in === true){
+    			console.log(user[i].firstname, num++, user[i].favorites[j].Current, user[i].favorites[j].riverlevel, user[i].favorites[j].riverInfo.name)
+          msg.push(user[i].favorites[j].riverInfo.name + ' is in! ' 
+                              + 'The Current flow is: '+ user[i].favorites[j].Current + '. Your recommended level is: ' + user[i].favorites[j].riverlevel + '.' + '\n')
+         
+    		};
+    	};
+    	var greeting = '\n' + 'Thank you for using theRiverReport'
+	
+    	console.log('sending msg to:', user[i].userphone, msg.join(' '))
+	     	if(msg.length>0){
+					client.messages.create({
+					    to: user[i].userphone, 
+					    from:"+19707103508",
+					    body: msg.join(' ') + greeting,
+					}, function(error, message) {
+					    if (error) {
+					        console.log(error.message);
+					    }
+					  });
+				}
+			}
 	},
 
 
@@ -252,6 +206,3 @@ var app = {
 }
 
 app.run()
-
-
-
