c144fce9e966bda6cbf5737f0b07f2992f34c8d5
paulsiskind <paulsiskind@gmail.com>
2017-08-24 09:50:14 -0600

    Rewriting River Page

--- public/javascripts/controllers/indexcontroller.js
+++ public/javascripts/controllers/indexcontroller.js
@@ -1,4 +1,4 @@
-  app.controller("IndexController", function($scope, $http, $location){
+  app.controller("IndexController", function($scope, $http, $location, $timeout, $mdSidenav, $log){
       
        // $scope.riverId = $routeParams.riverId;
        // console.log($routeParams.riverId)
@@ -85,4 +85,59 @@
         $scope.showflag = false;
       });
     }, 1500);
-  })
+  
+
+     $scope.toggleLeft = buildDelayedToggler('left');
+
+    /**
+     * Supplies a function that will continue to operate until the
+     * time is up.
+     */
+    function debounce(func, wait, context) {
+      var timer;
+
+      return function debounced() {
+        var context = $scope,
+            args = Array.prototype.slice.call(arguments);
+        $timeout.cancel(timer);
+        timer = $timeout(function() {
+          timer = undefined;
+          func.apply(context, args);
+        }, wait || 10);
+      };
+    }
+
+    /**
+     * Build handler to open/close a SideNav; when animation finishes
+     * report completion in console
+     */
+    function buildDelayedToggler(navID) {
+      return debounce(function() {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      }, 200);
+    }
+  
+   $scope.close = function () {
+      // Component lookup should always be available since we are not using `ng-if`
+      $mdSidenav('left').close()
+        .then(function () {
+          $log.debug("close LEFT is done");
+        });
+
+    };
+
+}) 
+.controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+  $scope.close = function () {
+    // Component lookup should always be available since we are not using `ng-if`
+    $mdSidenav('left').close()
+      .then(function () {
+        $log.debug("close LEFT is done");
+      });
+  };
+})
