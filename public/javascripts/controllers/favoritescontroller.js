aee0965bbe488ab3c2856f6ac715358e4dcf0cb5
paulsiskind <paulsiskind@gmail.com>
2017-08-23 21:39:49 -0600

    Updated Favorites page with side nav

--- public/javascripts/controllers/favoritescontroller.js
+++ public/javascripts/controllers/favoritescontroller.js
@@ -1,4 +1,4 @@
-app.controller('FavoritesController', function ($scope, $http, $routeParams, $rootScope, $window, $cookies) {
+app.controller('FavoritesController', function ($scope, $http, $routeParams, $rootScope, $window, $cookies, $timeout, $mdSidenav) {
   $scope.user = $cookies.getAll();
   $scope.flows = {};
   $scope.ideal = []
@@ -21,6 +21,8 @@ app.controller('FavoritesController', function ($scope, $http, $routeParams, $ro
       return 'Off'
     }
   }
+
+  
 
   $http.get('/userFavorites').then(function (response) {
     $scope.favorites = response.data
@@ -122,4 +124,76 @@ app.controller('FavoritesController', function ($scope, $http, $routeParams, $ro
     $scope.setBackground()
     $scope.$apply(); 
   },30000)
-});
+
+  ///////////////
+
+    $scope.toggleLeft = buildDelayedToggler('left');
+    $scope.toggleRight = buildToggler('right');
+    $scope.isOpenRight = function(){
+      return $mdSidenav('right').isOpen();
+    };
+
+    /**
+     * Supplies a function that will continue to operate until the
+     * time is up.
+     */
+    function debounce(func, wait, context) {
+      var timer;
+
+      return function debounced() {
+        var context = $scope,
+            args = Array.prototype.slice.call(arguments);
+        $timeout.cancel(timer);
+        timer = $timeout(function() {
+          timer = undefined;
+          func.apply(context, args);
+        }, wait || 10);
+      };
+    }
+
+    /**
+     * Build handler to open/close a SideNav; when animation finishes
+     * report completion in console
+     */
+    function buildDelayedToggler(navID) {
+      return debounce(function() {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      }, 200);
+    }
+
+    function buildToggler(navID) {
+      return function() {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      };
+    }
+  
+   $scope.close = function () {
+      // Component lookup should always be available since we are not using `ng-if`
+      $mdSidenav('left').close()
+        .then(function () {
+          $log.debug("close LEFT is done");
+        });
+
+    };
+
+}) 
+.controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+  $scope.close = function () {
+    // Component lookup should always be available since we are not using `ng-if`
+    $mdSidenav('left').close()
+      .then(function () {
+        $log.debug("close LEFT is done");
+      });
+  };
+})
+ 
